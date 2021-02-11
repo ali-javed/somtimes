6f4e4d548cc3f722d2a8115d4b9ba5646c9e1fba
Ali Javed <32461180+ali-javed@users.noreply.github.com>
2021-02-12 01:48:03 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# somtimes
+# Time Series Self Organizing Map (Somtimes)
+
+
+
+## Other relevant papers
+If you use this code please cite the time series clustering benchmark
+
+Ali Javed, Byung Suk Lee, and Donna M. Rizzo. “A benchmark study on time series clustering”. In: Machine Learning with Applications 1 (2020), p. 100001. issn: 2666-8270. doi: https://doi.org/10.1016/j.mlwa.2020.100001. url: http://www.sciencedirect.com/science/article/pii/S2666827020300013
+
+
+
+
+
+## Installation
+
+somtime is available on PyPI 
+
+```console
+$ pip install somtimes
+```
+
+### Install from source
+
+
+```console
+$ python setup.py install
+```
+
+## Usage
+
+
+```
+see demo.py for example and explaination of usage
+The SOM can cluster time series data.
+
+```
---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import setuptools
+
+__version__ = "1.0.0"
+
+
+CLASSIFIERS = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Natural Language :: English",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3.2",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Topic :: Scientific/Engineering :: Information Analysis",
+    "Topic :: Scientific/Engineering :: Mathematics",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
+
+setuptools.setup(
+    name="somtimes",
+    version=__version__,
+    description="Python implementation of Self Organizing Map with capability to use Dynamic Time Warping (DTW) as a distance measure.",
+    long_description = "",
+    author="A. Javed",
+    author_email="alijaved@live.com",
+    packages=setuptools.find_packages(),
+    zip_safe=True,
+    license="",
+    download_url = "https://github.com/ali-javed/somtimes/archive/1.0.8.tar.gz",
+    url="https://github.com/ali-javed/somtimes",
+    install_requires=['numpy','matplotlib']
+)
